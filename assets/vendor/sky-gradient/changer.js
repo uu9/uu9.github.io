e126f1d5abf3de66f3204d09f3654fa6887c5aaa
uu9 <3395538560@qq.com>
2020-03-22 16:00:35 +0800

    Update changer.js

--- assets/vendor/sky-gradient/changer.js
+++ assets/vendor/sky-gradient/changer.js
@@ -2,6 +2,6 @@
 var o = document.getElementsByClassName("overlay")[0];
 var d = new Date();
 var dhours = d.getHours();
-o.style.backgroundColor = "linear-gradient(to bottom, #020111 10%, #3a3a52 100%)";
+o.style.background = "linear-gradient(to bottom, #020111 10%, #3a3a52 100%)";
 console.log("ok");
 </script>
